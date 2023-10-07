b6bf58941c619aaa2c89b7d9d802141cf236077a
Akhpie <akhya1819@gmail.com>
2023-10-07 21:36:11 +0530

    changesss

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -129,7 +129,7 @@ export default function ProductBox({
       <ProductInfoBox>
         <Title href={url}>{title}</Title>
         <PriceRow>
-          <Price>${price}</Price>
+          <Price>₹{price}</Price>
           <Button block onClick={() => addProduct(_id)} primary outline>
             Add to cart
           </Button>
